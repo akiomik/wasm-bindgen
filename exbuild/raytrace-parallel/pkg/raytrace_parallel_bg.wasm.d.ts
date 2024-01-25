d63d262016ae6abf27d097e68edff259eb4236f0
Liamolucko <liampm32@gmail.com>
2024-01-25 09:11:18 +0000

    Deploying to gh-pages from @ rustwasm/wasm-bindgen@acef364c7ed4d4a32e31f00a7a834d03c4900eb7 🚀

---
+++ exbuild/raytrace-parallel/pkg/raytrace_parallel_bg.wasm.d.ts
@@ -0,0 +1,23 @@
+/* tslint:disable */
+/* eslint-disable */
+export function __wbg_workerpool_free(a: number): void;
+export function workerpool_new(a: number, b: number): void;
+export function child_entry_point(a: number, b: number): void;
+export function __wbg_scene_free(a: number): void;
+export function scene_new(a: number, b: number): void;
+export function scene_render(a: number, b: number, c: number, d: number): void;
+export function __wbg_renderingscene_free(a: number): void;
+export function renderingscene_promise(a: number): number;
+export function renderingscene_imageSoFar(a: number): number;
+export const memory: WebAssembly.Memory;
+export function __wbindgen_malloc(a: number, b: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number, d: number): number;
+export const __wbindgen_export_3: WebAssembly.Table;
+export function wasm_bindgen__convert__closures__invoke1_mut__ha0645d80fe06b308(a: number, b: number, c: number): void;
+export function _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h5459e42e2a21c8c3(a: number, b: number, c: number): void;
+export function __wbindgen_add_to_stack_pointer(a: number): number;
+export function __wbindgen_exn_store(a: number): void;
+export function __wbindgen_free(a: number, b: number, c: number): void;
+export function wasm_bindgen__convert__closures__invoke2_mut__h40131155fe433d6c(a: number, b: number, c: number, d: number): void;
+export function __wbindgen_thread_destroy(a: number, b: number): void;
+export function __wbindgen_start(): void;
